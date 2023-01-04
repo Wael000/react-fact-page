920b859635d15e91c20ae311ba44474674905887
Wael <wael_ben_ayed@yahoo.com>
2023-01-04 23:24:47 +0100

    1st commit

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main(props) {
+    return(
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main-title">Fun facts about React</h1>
+            <ul className="main-facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Has over 100k stars in Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile app</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function WindowTracker(props) {
+    return(
+        <nav 
+            className={props.darkMode ? "dark" : ""}
+        >
+            <img 
+                className="nav--logo_icon"
+                src="./images/react-icon-small.svg.png"
+            />
+            <h3 className="nav--logo_text">React Facts</h3>
+            <div
+                className="toggler"
+            >
+                <p className="toggler--light">Light</p>
+                <div
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
